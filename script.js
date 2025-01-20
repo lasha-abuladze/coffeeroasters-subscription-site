9c8e9d79e3ea1746829e3bc0676fe09020d60da2
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2025-01-20 21:12:55 +0400

    add animations on the menu button. burger menu gradually turns into a X shape after click evet

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 `use strict`;
 
-const menuBtn = document.querySelector(`.menu-btn`);
-const closeBtn = document.querySelector(`.close-btn`);
+// const menuBtn = document.querySelector(`.menu-btn`);
+// const closeBtn = document.querySelector(`.close-btn`);
+
+
+// const togleMenuBtn = function(menuBtn, closeBtn) {
+//     menuBtn.classList.toggle(`display-none`);
+//     closeBtn.classList.toggle(`display-none`);
+// }
+
+// menuBtn.addEventListener(`click`, () => {
+//     togleMenuBtn(menuBtn, closeBtn)
+// } )
+
+// closeBtn.addEventListener(`click`, () => {
+//     togleMenuBtn(menuBtn, closeBtn)
+// })
 
 
 
 
-const togleMenuBtn = function(menuBtn, closeBtn) {
-    menuBtn.classList.toggle(`display-none`);
-    closeBtn.classList.toggle(`display-none`);
-}
+const menuButton = document.querySelector(`.close-open--btns`);
+
+const line1 = document.querySelector(`.line--1`);
+const line2 = document.querySelector(`.line--2`);
+const line3 = document.querySelector(`.line--3`);
 
 
-
-
-
-
-
-menuBtn.addEventListener(`click`, () => {
-    togleMenuBtn(menuBtn, closeBtn)
-} )
-
-closeBtn.addEventListener(`click`, () => {
-    togleMenuBtn(menuBtn, closeBtn)
+menuButton.addEventListener(`click`, () => {
+    line1.classList.add(`line--1__close`);
+    line2.classList.add(`line--2__close`);
+    line3.classList.add(`line--3__close`);
 })
