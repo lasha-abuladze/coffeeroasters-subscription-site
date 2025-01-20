0e7b771b78d5c235fb61fc6bc46d7723f06036a7
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2025-01-20 23:53:05 +0400

    finish menu button animation

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
 `use strict`;
 
-// const menuBtn = document.querySelector(`.menu-btn`);
-// const closeBtn = document.querySelector(`.close-btn`);
-
-
-// const togleMenuBtn = function(menuBtn, closeBtn) {
-//     menuBtn.classList.toggle(`display-none`);
-//     closeBtn.classList.toggle(`display-none`);
-// }
-
-// menuBtn.addEventListener(`click`, () => {
-//     togleMenuBtn(menuBtn, closeBtn)
-// } )
-
-// closeBtn.addEventListener(`click`, () => {
-//     togleMenuBtn(menuBtn, closeBtn)
-// })
-
-
-
-
 const menuButton = document.querySelector(`.close-open--btns`);
 
 const line1 = document.querySelector(`.line--1`);
@@ -27,8 +7,29 @@ const line2 = document.querySelector(`.line--2`);
 const line3 = document.querySelector(`.line--3`);
 
 
+
+
+
 menuButton.addEventListener(`click`, () => {
-    line1.classList.add(`line--1__close`);
-    line2.classList.add(`line--2__close`);
-    line3.classList.add(`line--3__close`);
+
+
+    if(!line1.classList.contains(`line--1__close`)) {
+
+        line1.classList.remove(`line--1__burger`);
+        line2.classList.remove(`line--2__burger`);
+        line3.classList.remove(`line--3__burger`);
+
+        line1.classList.add(`line--1__close`);
+        line2.classList.add(`line--2__close`);
+        line3.classList.add(`line--3__close`);
+    } else {
+        line1.classList.remove(`line--1__close`);
+        line2.classList.remove(`line--2__close`);
+        line3.classList.remove(`line--3__close`);
+
+        line1.classList.add(`line--1__burger`);
+        line2.classList.add(`line--2__burger`);
+        line3.classList.add(`line--3__burger`);
+    }
+
 })
