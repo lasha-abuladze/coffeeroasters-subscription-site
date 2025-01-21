d1b9ed26fd36b074fea3aa2fcd841910bca66256
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2025-01-21 22:02:53 +0400

    finish navigation animations

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ const line3 = document.querySelector(`.line--3`);
 
 
 
+const liHome = document.querySelector(`.li--home`);
+const liabout = document.querySelector(`.li--about`);
+const licreate = document.querySelector(`.li--create`);
+
+const navigation = document.querySelector(`.main--navigation`)
+
+
+
 
 
 menuButton.addEventListener(`click`, () => {
@@ -22,6 +30,18 @@ menuButton.addEventListener(`click`, () => {
         line1.classList.add(`line--1__close`);
         line2.classList.add(`line--2__close`);
         line3.classList.add(`line--3__close`);
+
+
+        liHome.classList.remove(`li-home--close`);
+        liabout.classList.remove(`li-about--close`);
+        licreate.classList.remove(`li-create--close`);       
+
+        liHome.classList.add(`li-home--open`);
+        liabout.classList.add(`li-about--open`);
+        licreate.classList.add(`li-create--open`);
+
+        navigation.style.display = `block`;
+
     } else {
         line1.classList.remove(`line--1__close`);
         line2.classList.remove(`line--2__close`);
@@ -30,6 +50,21 @@ menuButton.addEventListener(`click`, () => {
         line1.classList.add(`line--1__burger`);
         line2.classList.add(`line--2__burger`);
         line3.classList.add(`line--3__burger`);
+
+        liHome.classList.remove(`li-home--open`);
+        liabout.classList.remove(`li-about--open`);
+        licreate.classList.remove(`li-create--open`);
+
+        liHome.classList.add(`li-home--close`);
+        liabout.classList.add(`li-about--close`);
+        licreate.classList.add(`li-create--close`);
+
+        const intervalId = setInterval(() => {
+            navigation.style.display = 'none';
+          
+            clearInterval(intervalId);
+          }, 1000);
+        
     }
 
 })
